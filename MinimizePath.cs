b4a159e42a62af701be9d704405f458f8a9cf05d
asafs <asafs@mythings.com>
2014-01-05 09:53:19 +0200

    Initial commit for Environment Path Minimize project

---
+++ MinimizePath.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MinimizeEnvironmentVariables
+{
+    class MinimizePath
+    {
+        static void Main(string[] args)
+        {
+            const string environmentVariableName = "path";
+            String oldPathVal = Environment.GetEnvironmentVariable(environmentVariableName, EnvironmentVariableTarget.Machine);
+            String newVal = RemoveUnusedAndDuplicateValues(oldPathVal, ';');
+            Environment.SetEnvironmentVariable(environmentVariableName, newVal, EnvironmentVariableTarget.Machine);
+
+            int removedCharacters = oldPathVal.Length - newVal.Length ;
+            Console.WriteLine("Path environment variable changed.\n" +
+                              "Removed "+ removedCharacters+" characters." );
+           }
+     
+        /// <summary>
+        /// Remove duplicates and removes paths that doesn't exist on the system
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="delimeter"></param>
+        /// <returns></returns>
+        public static String RemoveUnusedAndDuplicateValues(String list, char delimeter)
+        {
+            var newList = new List<String>();
+            foreach (var elem in list.Split(delimeter)) // Remove duplicates
+            {
+                var value = elem.ToLower();
+                if (!newList.Contains(value))
+                    newList.Add(value);
+            }
+            
+            return newList.Where(Directory.Exists).Aggregate((first, last) => first + delimeter + last); //Return only existing directories
+            
+       }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,18 @@
+
+MinimizePath
+============
+In windows 7 there is a max number of characters allowed to be in system's PATH environment variable.
+This caused me some trouble, because I use many programs that set their paths there.
+
+MinimizePath application is a small application created to reduce the number of characters in 
+the PATH environment variable of windows 7.
+
+you can change it very easily for your own use,
+Hope you will find it useful.
+
+wasafa1@gmail.com
+
+Running Instructions
+====================
+Open command line (Start -> cmd.exe) and run the MinimizeEnvironmentVariables.exe file.
+that's it.
